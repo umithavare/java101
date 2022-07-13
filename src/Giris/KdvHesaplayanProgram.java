6cf4027642d3ef7b2f01482374d720ea61c5a9ee
umith <umithavare@gmail.com>
2022-07-13 17:59:13 -0500

    kdv hesaplayan program eklendi.

---
+++ src/Giris/KdvHesaplayanProgram.java
@@ -0,0 +1,16 @@
+package Giris;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class KdvHesaplayanProgram {
+    public static void main(String[] args) {
+        double fiyat,kdvliFiyat,kdvTutari;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen ürün Fiyati giriniz: ");
+        fiyat = input.nextDouble();
+        kdvTutari = fiyat * 0.18;
+        kdvliFiyat= fiyat + kdvTutari;
+        System.out.println("Kdv Miktari %18'dir.");
+        System.out.println("Kdv'siz Fiyat :\t" +fiyat + "\nKdv Tutarı :\t"+ kdvTutari + "\nKdv'li fiyat :\t" + kdvliFiyat);
+    }
+}
