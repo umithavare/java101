3b82078bf6c27b71fad59715fc00c2fbbbc7496b
umith <umithavare@gmail.com>
2022-07-13 17:26:37 -0500

    not ort hesaplayan program eklendi

--- src/Giris/Baslangic.java
+++
@@ -1,8 +0,0 @@
-package Giris;
-
-public class Baslangic {
-    public static void main(String[] args) {
-        float sayi = 3 ;
-        System.out.println(sayi);
-    }
-}
---
+++ src/Giris/NotOrtHesaplayanProgram.java
@@ -0,0 +1,29 @@
+package Giris;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class NotOrtHesaplayanProgram {
+    public static void main(String[] args) {
+        // Değişkenler oluşturuldu.
+        int mat,fzk,kim,trk,trh,mzk,ortalama;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Matematik Dersini Notunu giriniz: ");
+        mat = input.nextInt();
+        System.out.println("Fizik Dersinin notunu giriniz: ");
+        fzk = input.nextInt();
+        System.out.println("Kimya Dersinin notunu giriniz: ");
+        kim = input.nextInt();
+        System.out.println("Türkçe Dersinin notunu giriniz: ");
+        trk = input.nextInt();
+        System.out.println("Tarih Dersinin notunu giriniz: ");
+        trh = input.nextInt();
+        System.out.println("Müzik Dersinin notunu Giriniz");
+        mzk = input.nextInt();
+        // bütün inputlar alındı
+        ortalama = (mat + fzk + kim + trk + trh + mzk) / 6 ; //ortalama hesaplandı
+        boolean gecme = ortalama >= 60;
+        String sonuc = gecme ? "Gecti" : "Kaldi";
+
+        System.out.println("Ortalamanız:\t" +ortalama  + "\nGeçme Durumunuz:\t" +sonuc);
+
+    }
+}
